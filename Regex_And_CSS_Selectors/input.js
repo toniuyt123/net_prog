e9f5bed02c7ffafed44999edf8e79b1901924345
toniuyt123 <antoniomilev123@gmail.com>
2018-12-04 19:50:21 +0200

    homework 4

---
+++ Regex_And_CSS_Selectors/input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /Internet|Words|Or/,
+"exercise_2": /088[1-9]{7}/,
+"exercise_3": /[^0-1]+/,
+"exercise_4": /^([a-z]|[A-Z])[\w.]{2,20}$/,
+"exercise_5": /^\d{0,3}$|^[1][0-4]\d{2}$|(1500)/,
+"exercise_6": /class=["'][\w ]+['"]/
+};
+cssSelectors = {
+"exercise_1": "item > java",
+"exercise_2": "different java",
+"exercise_3": ".class1 > .class1.class2",
+"exercise_4": "css > item + item + item",
+"exercise_5": ".class1.class2 > .class1 + .class1",
+"exercise_6": "item > item > item > item > item",
+"exercise_7": "#diffId2 > .diffClass + .diffClass",
+"exercise_8": "#someId"
+};

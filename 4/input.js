5ff61e13884dde81e077cea56add830f3be6ee5a
toniuyt123 <antoniomilev123@gmail.com>
2018-12-09 20:28:54 +0200

    update

--- 4/input.js
+++ 4/input.js
@@ -1,8 +1,8 @@
 regExps = {
-"exercise_1": /Internet|Words|Or/,
-"exercise_2": /088[1-9]{7}/,
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1-9][0-9]{6}/,
 "exercise_3": /[^0-1]+/,
-"exercise_4": /^([a-z]|[A-Z])[\w.]{2,20}$/,
+"exercise_4": /^[a-zA-Z][\w.]{2,20}$/,
 "exercise_5": /^\d{0,3}$|^[1][0-4]\d{2}$|(1500)/,
 "exercise_6": /class=["'][\w ]+['"]/
 };
